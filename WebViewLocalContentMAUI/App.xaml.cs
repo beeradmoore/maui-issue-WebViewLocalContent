58eec383c964e47843c0709069a06fe88084629c
Brad <brad@4pi.com.au>
2024-02-01 15:36:45 +1100

    Initial commit of MAUI project

---
+++ WebViewLocalContentMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WebViewLocalContentMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
